2047b0d959f9906e0fa540244db7f5593d897b2e
Mstrutov <strutovsky.m.a@gmail.com>
2022-02-23 21:48:36 +0300

    Implement orange part core
    - add methods and fields from the structural diagram
    - implement command parser logic

--- hw1/src/execution/Executor.java
+++ hw1/src/execution/Executor.java
@@ -1,5 +1,16 @@
 package execution;
 
-public class Executor {
+import java.util.List;
+import jdk.jshell.spi.ExecutionControl.NotImplementedException;
 
+public class Executor {
+    private final String buffer = ""; // TODO: Executables должны иметь доступ к buffer. Либо execute() возвращать String
+
+    ResultCode execute(List<Executable> inStmts) throws NotImplementedException {
+        throw new NotImplementedException("Not yet");
+    }
+
+    String getBuffer() {
+        return buffer;
+    }
 }
