a02a54159c704b6ef82ba4629185d39ab4c7dc62
Mstrutov <strutovsky.m.a@gmail.com>
2022-02-25 13:52:47 +0300

    add CommandParser tests

--- hw1/src/main/java/environment/CommandRegistry.java
+++ hw1/src/main/java/environment/CommandRegistry.java
@@ -1,10 +1,11 @@
 package environment;
 
 import execution.commands.BuiltInCmd;
+import java.util.HashMap;
 import java.util.Map;
 
 public class CommandRegistry {
-    private Map<String, BuiltInCmd> cmdEnvironment;
+    private Map<String, BuiltInCmd> cmdEnvironment = new HashMap<>();
 
     //TODO: либо наполнять в конструкторе NinB, либо как-нибудь через static придумать
     public BuiltInCmd getCmd(String cmdName) {
