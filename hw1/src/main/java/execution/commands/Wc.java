a527d8fe19aabb8666565bde19cfffd004c40aa3
Moroz_Nikolaj <284664@niuitmo.ru>
2022-03-06 02:20:00 +0300

    minor fixes

--- hw1/src/main/java/execution/commands/Wc.java
+++ hw1/src/main/java/execution/commands/Wc.java
@@ -10,12 +10,12 @@ import java.io.*;
 public class Wc implements BuiltInCmd {
     @Override
     public ResultCode execute(String[] args, StringBuilder buffer) {
-        try {
-            if (args.length == 0 && buffer.isEmpty()) {
-                System.err.println("Wc required at least 1 argument or non-empty buffer");
-                return new ResultCode(1, false);
-            }
-            BufferedReader br = new BufferedReader(buffer.isEmpty() ? new FileReader(args[0]) : new StringReader(buffer.toString()));
+
+        if (args.length == 0 && buffer.isEmpty()) {
+            System.err.println("Wc required at least 1 argument or non-empty buffer");
+            return new ResultCode(1, false);
+        }
+        try (BufferedReader br = new BufferedReader(buffer.isEmpty() ? new FileReader(args[0]) : new StringReader(buffer.toString()));) {
             buffer.setLength(0);
             int lineCounter = 0;
             while (br.readLine() != null) {
