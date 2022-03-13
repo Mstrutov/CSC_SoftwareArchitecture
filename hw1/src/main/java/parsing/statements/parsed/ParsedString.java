1177c04789057659fda138d309be7d751acff72c
Mstrutov <strutovsky.m.a@gmail.com>
2022-03-13 22:24:09 +0600

    Add missing javadoc comments

--- hw1/src/main/java/parsing/statements/parsed/ParsedString.java
+++ hw1/src/main/java/parsing/statements/parsed/ParsedString.java
@@ -4,5 +4,8 @@ package parsing.statements.parsed;
  * Parsed with REPL String
  */
 public interface ParsedString {
+    /**
+     * @return String representation
+     */
     String getString();
 }
